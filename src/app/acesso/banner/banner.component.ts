672f5b0445b14fb89fa24bad179566025aa44da9
Done Ribeiro <www.done_@hotmail.com>
2022-04-24 10:55:00 -0300

    Implementing banner logic

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -23,20 +23,38 @@ export class BannerComponent implements OnInit {
   public estado: string = 'visivel'
 
   public imagens: Imagem[] = [
-    { estado: 'escondido', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
     { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
     { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
-    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' }
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png' }
   ]
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.imagens)
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
-  public toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
+  public logicaRotacao(): void {
+    //auxilia na exibição da imagem seguinte
+    let idx!: number
+
+    //ocultar imagem
+    for (let i: number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+
+        idx = i === 4 ? 0 : i + 1
+
+        break
+      }
+    }
+
+    //exibir a próxima imagem
+    this.imagens[idx].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
 }
