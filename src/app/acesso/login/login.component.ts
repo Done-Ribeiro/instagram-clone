1bdd867f4f445cd2382686bfff920ea144f2052e
Done Ribeiro <www.done_@hotmail.com>
2022-04-28 13:25:07 -0300

    CHALLENGE 5 - login

--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 
 import { Autenticacao } from 'src/app/autenticacao.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('animacao-login', [
+      state('criado', style({
+        opacity: 1
+      })),
+      state('erro', style({
+        opacity: 1
+      })),
+      transition('criado => erro', [
+        animate('200ms', keyframes([
+          style({ offset: 0.25, transform: 'translateX(-10px)' }),
+          style({ offset: 0.50, transform: 'translateX(10px)' }),
+          style({ offset: 0.75, transform: 'translateX(-10px)' }),
+          style({ offset: 0.99, transform: 'translateX(10px)' }),
+        ]))
+      ])
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
+
+  public estadoLogin: string = 'criado'
 
   public formulario: FormGroup = new FormGroup({
     'email': new FormControl(null, [
@@ -39,7 +60,10 @@ export class LoginComponent implements OnInit {
       this.formulario.value.email,
       this.formulario.value.senha
     )
-      .then((resposta: any) => this.mensagem = resposta)
+      .then((resposta: any) => {
+        this.mensagem = resposta
+        this.estadoLogin = 'erro'
+      })
   }
 
 }
