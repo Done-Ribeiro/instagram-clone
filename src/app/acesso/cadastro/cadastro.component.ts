13c8e778db1543538157d7396f87358fc30c2920
Done Ribeiro <www.done_@hotmail.com>
2022-04-28 13:49:30 -0300

    CHALLENGE 5 - register

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 import { Usuario } from '../usuario.model';
 import { Autenticacao } from 'src/app/autenticacao.service'
@@ -7,13 +8,29 @@ import { Autenticacao } from 'src/app/autenticacao.service'
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
+  animations: [
+    trigger('animacao-cadastro', [
+      state('criado', style({})),
+      state('erro', style({})),
+      transition('criado => erro', [
+        animate('200ms', keyframes([
+          style({ offset: 0.25, transform: 'translateX(10px)' }),
+          style({ offset: 0.50, transform: 'translateX(-10px)' }),
+          style({ offset: 0.75, transform: 'translateX(10px)' }),
+          style({ offset: 0.99, transform: 'translateX(-10px)' })
+        ]))
+      ])
+    ])
+  ]
 })
 export class CadastroComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
 
   public mensagem!: any
+
+  public estadoCadastro: string = 'criado'
 
   public formulario: FormGroup = new FormGroup({
     'email': new FormControl(null, [
@@ -53,6 +70,7 @@ export class CadastroComponent implements OnInit {
       .then((erro: Error) => {
         if (erro) {
           this.mensagem = erro
+          this.estadoCadastro = 'erro'
         } else {
           this.exibirPainelLogin()
         }
