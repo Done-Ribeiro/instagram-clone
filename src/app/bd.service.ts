a5b8910ebe89c0f318337ca2d17194788c646dbf
Done Ribeiro <www.done_@hotmail.com>
2022-04-29 18:04:31 -0300

    Adjusting the recording of the image (Storage) and title (Database) of the publication

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -11,34 +11,37 @@ export class Bd {
   ) { }
 
   public publicar(publicacao: any): void {
-    console.log(publicacao)
+    // console.log(publicacao)
 
-    let nomeImagem = Date.now()
+    //faremos este processo aqui, para em seguida garantir uma imagem unica no storage...
+    firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+      .push({ titulo: publicacao.titulo })
+      .then((resposta: any) => {
+        //enviando a KEY para o storage, garantimos que a imagem tera um nome unico
+        //pois agora o nome da imagem, sera o valor da KEY
+        let nomeImagem = resposta.key
 
-    //upload pro storage do firebase
-    firebase.storage().ref()
-      .child(`imagens/${nomeImagem}`)
-      .put(publicacao.imagem)
-      .on(firebase.storage.TaskEvent.STATE_CHANGED,
-        //acompanhamento do progresso do upload
-        (snapshot: any) => {
-          this.progresso.status = 'andamento'
-          this.progresso.estado = snapshot
-          // console.log('Snapshot capturado no on(): ', snapshot)
-        },
-        (erro: Error) => {
-          this.progresso.status = 'erro'
-          // console.log(erro)
-        },
-        () => {
-          //finalização do processo
-          this.progresso.status = 'concluido'
-          // console.log('upload completo')
-        }
-      )//listener on(recebe um evento)
-
-
-    // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-    //   .push( { titulo: publicacao.titulo } )
+        //upload pro storage do firebase
+        firebase.storage().ref()
+          .child(`imagens/${nomeImagem}`)
+          .put(publicacao.imagem)
+          .on(firebase.storage.TaskEvent.STATE_CHANGED,
+            //acompanhamento do progresso do upload
+            (snapshot: any) => {
+              this.progresso.status = 'andamento'
+              this.progresso.estado = snapshot
+              // console.log('Snapshot capturado no on(): ', snapshot)
+            },
+            (erro: Error) => {
+              this.progresso.status = 'erro'
+              // console.log(erro)
+            },
+            () => {
+              //finalização do processo
+              this.progresso.status = 'concluido'
+              // console.log('upload completo')
+            }
+          )//listener on(recebe um evento)
+      })
   }
 }
