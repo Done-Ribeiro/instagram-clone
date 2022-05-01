a9e6a88756f1d75dce114ebca5c493940dede0a3
Done Ribeiro <www.done_@hotmail.com>
2022-05-01 16:02:44 -0300

    Updating the timeline after adding a new post - ViewChild

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Autenticacao } from '../autenticacao.service'
 
@@ -8,6 +8,8 @@ import { Autenticacao } from '../autenticacao.service'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild('publicacoes') public publicacoes: any
 
   constructor(
     private autenticacao: Autenticacao
@@ -21,7 +23,7 @@ export class HomeComponent implements OnInit {
   }
 
   public atualizarTimeLine(): void {
-    console.log('chegamos até aqui!')
+    this.publicacoes.atualizarTimeLine()
   }
 
 }
